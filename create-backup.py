0fd804b2577ea8bdd9d6de231118d0c94732ecc1
Nicole Nisters <n.nisters@pm.me>
2018-11-13 11:57:44 +0100

    Initial commit

---
+++ create-backup.py
@@ -0,0 +1,156 @@
+"""Create an encrypted .tar.gz archive,
+   which can be burned to a cd/dvd or moved somewhere else."""
+
+# TODO Backup Pi?
+# TODO Option to restore backup
+# TODO Improve input and output, use logging for messages?
+# TODO Read directories from .cfg file
+# TODO Set recipients in .cfg file
+# TODO Set gnupg home in .cfg
+# TODO Set output as argument
+# TODO Handle missing file errors
+
+import argparse
+import sys
+import os
+import tarfile
+import datetime
+import gnupg
+import getpass
+import logging as log
+import configparser
+
+def update_progress_bar(current, total, msg=''):
+    """Display a progress bar."""
+    bar_length = 10
+    progress = current / total
+    blocks = int(round(bar_length * progress))
+    text = '\rProgress: {} {}/{} {:.1f}% {}'\
+            .format('▇' * blocks + '-' * (bar_length - blocks),
+                    current, total, progress * 100, msg)
+    if progress == 1:
+        text += '\n'
+
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return '%3.1f %s%s' % (num, unit, suffix)
+        num /= 1024.0
+    return '%.1f %s%s' % (num, 'Yi', suffix)
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description="""Create a backup."""
+    )
+    parser.add_argument(
+        '-c', '--critical',
+        help="Create archive containing critical directories",
+        action='store_true'
+    )
+    parser.add_argument(
+        '-i', '--important',
+        help="Create archive containing important directories",
+        action='store_true'
+    )
+    parser.add_argument(
+        '-n', '--nonessential',
+        help="Create archive containing non-essential directories",
+        action='store_true'
+    )
+    parser.add_argument(
+        '-s', '--symmetric',
+        help="Use symmetric encryption",
+        action='store_true'
+    )
+    args = parser.parse_args(argv)
+    directories = []
+    log.basicConfig(format='%(asctime)s %(message)s', level=log.INFO)
+    
+    passphrase = ''
+    if args.symmetric:
+        valid = False
+        while not valid:
+            passphrase = getpass.getpass('Passphrase to use: ')
+            confirm = getpass.getpass('Re-type your passphrase: ')
+
+            if passphrase == confirm:
+                valid = True
+                print('Passphrases match.')
+            else:
+                print('Passphrases do not match.')
+
+    if args.critical:
+        directories.extend(DIRECTORIES_CRITICAL)
+
+    if args.important:
+        directories.extend(DIRECTORIES_IMPORTANT)
+
+    if args.nonessential:
+        directories.extend(DIRECTORIES_NON_ESSENTIAL)
+
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    filename = '/tmp/backup-{}.tar.gz'.format(date)
+    longest_dir_length = 0
+    total_size = 0
+    for directory in directories:
+        total_size = total_size + get_size(directory)
+        if len(directory) > longest_dir_length:
+            longest_dir_length = len(directory)
+
+    print('Archiving {} directories with total size of {}.'
+          .format(len(directories), sizeof_fmt(total_size)))
+
+    with tarfile.open(filename, 'w:gz') as tar:
+        for directory in enumerate(directories):
+            tar.add(directory[1], arcname=os.path.basename(directory[1]))
+
+            padding = ' ' * (longest_dir_length - len(directory[1]))
+
+            update_progress_bar(directory[0], len(directories),
+                                directory[1] + padding)
+
+    update_progress_bar(len(directories), len(directories),
+                        ' ' * longest_dir_length)
+
+    archived_size = sizeof_fmt(os.path.getsize(filename))
+    log.info('Archiving complete. Resulting filesize: {}.'
+             .format(archived_size))
+
+    gpg = gnupg.GPG(gnupghome='/home/nicole/.gnupg')
+
+    log.info('Encrypting archive.')
+
+    with open(filename, 'rb') as f:
+        filename = filename + '.gpg'
+        status = gpg.encrypt_file(f, recipients=RECIPIENTS, output=filename, armor=False, symmetric=args.symmetric, passphrase=passphrase)
+
+    # TODO Handle errors
+
+    print('ok:', status.ok)
+    print('status:', status.status)
+    print('stderr:', status.stderr)
+
+    encrypted_size = sizeof_fmt(os.path.getsize(filename))
+    log.info('Encryption complete. Resulting filesize: {}.'.format(encrypted_size))
+    print("Backup '{}' complete.".format(filename))
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        print('Interrupted by user.')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0) # pylint: disable=protected-access
